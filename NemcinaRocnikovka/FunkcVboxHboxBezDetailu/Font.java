3cbfaadfc259e9026bec7ef24ee9e9b7a0ce9de5
KrystofMaxa <94890860+KrystofMaxa@users.noreply.github.com>
2023-01-15 21:57:39 +0100

    Add files via upload
    Funguje vypisovani do vboxu a ty se pak rpidaji do hboxu
    Musi se dodelat: Ohraniceni, Vybrani mezi moznostma 1234556, udelat hranici? a moznost scroll

---
+++ NemcinaRocnikovka/FunkcVboxHboxBezDetailu/Font.java
@@ -0,0 +1,8 @@
+package com.example.rocnikovkanemcina;
+
+public class Font {
+    public static Font font(double fontSize) {
+        Font font2 = Font.font(fontSize);
+        return font2;
+    }
+}
