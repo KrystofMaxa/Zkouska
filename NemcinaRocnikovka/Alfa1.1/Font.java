b7378bbce2c1094e5207b74f33ae1dd5c10feb93
KrystofMaxa <94890860+KrystofMaxa@users.noreply.github.com>
2023-03-11 14:23:30 +0100

    Add files via upload

---
+++ NemcinaRocnikovka/Alfa1.1/Font.java
@@ -0,0 +1,8 @@
+package com.example.rocnikovkanemcinaalfa_1_1;
+
+public class Font {
+    public static Font font(double fontSize) {
+        Font font2 = Font.font(fontSize);
+        return font2;
+    }
+}
